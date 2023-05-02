a580d9df4bb8cc1ecab9de2ee60ee3c38a251263
newbe01 <pkwaa3@gmail.com>
2023-05-03 03:10:40 +0900

    #1 - 스프링 부트 프로젝트 초기화
    어드민 프로젝트의 초기화 코드 작성, 정상 구동 확인

---
+++ src/main/java/com/forstudy/adminboard/AdminBoardApplication.java
@@ -0,0 +1,15 @@
+package com.forstudy.adminboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class AdminBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBoardApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/forstudy/adminboard/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.forstudy.adminboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //권한 검사
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
+                .build();
+    }
+
+}
