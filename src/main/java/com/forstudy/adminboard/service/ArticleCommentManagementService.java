05ba93322b960ff0132676dfe91a611428a89c79
newbe01 <pkwaa3@gmail.com>
2023-05-13 19:54:44 +0900

    #22-comment-management-test
    댓글 관리 페이지의 테스트를 미리 작성

---
+++ src/main/java/com/forstudy/adminboard/service/ArticleCommentManagementService.java
@@ -0,0 +1,25 @@
+package com.forstudy.adminboard.service;
+
+import com.forstudy.adminboard.dto.ArticleCommentDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleCommentManagementService {
+
+    public List<ArticleCommentDto> getArticleComments() {
+        return List.of();
+    }
+
+    public ArticleCommentDto getArticleComment(Long articleCommentId) {
+        return null;
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+
+    }
+
+}
