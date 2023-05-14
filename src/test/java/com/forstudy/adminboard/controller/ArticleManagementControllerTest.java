31c66e309538a7d7212785e6c87f52d493f0b3bc
newbe01 <pkwaa3@gmail.com>
2023-05-14 20:56:26 +0900

    #28 로그인페이지 테스트
    * 인증 규칙을 좀 더 세밀하게 설정, 모든 접근은 이제 인증을 필요로 함
    * 카카오 인증을 위한 준비
    * PasswordEncoder 등록
    * 어드민 회원 서비스 코드작성
    * enum description 추가
    * 어드민 회원 api 의 응답 객체 설계
    * 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트
    * TestSecurityConfg 작성
    이것으로 모든 controller test 에서 일일이
    `AdminAccountService`를 mocking 하거나,
    인증 세팅을 하는 코드를 표현하지 않아도 됨.
    각 컨트롤러 관심사에서 벗어나는 인증 코드 및 mocking이
    테스트 코드에 드러나지 않게끔 도와줌.

--- src/test/java/com/forstudy/adminboard/controller/ArticleManagementControllerTest.java
+++ src/test/java/com/forstudy/adminboard/controller/ArticleManagementControllerTest.java
@@ -1,7 +1,6 @@
 package com.forstudy.adminboard.controller;
 
-import com.forstudy.adminboard.config.SecurityConfig;
-import com.forstudy.adminboard.domain.constant.RoleType;
+import com.forstudy.adminboard.config.TestSecurityConfig;
 import com.forstudy.adminboard.dto.ArticleDto;
 import com.forstudy.adminboard.dto.UserAccountDto;
 import com.forstudy.adminboard.service.ArticleManagementService;
@@ -16,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -25,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("컨트롤러 - 게시글 관리")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(ArticleManagementController.class)
 class ArticleManagementControllerTest {
 
